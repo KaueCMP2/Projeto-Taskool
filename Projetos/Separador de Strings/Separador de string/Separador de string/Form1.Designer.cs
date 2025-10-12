c48c99435301be0fe7cdc0d5978bce48078bb2a8
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2025-10-11 21:19:55 -0300

    Projeto Nick aleatorio funcionando

---
+++ Projetos/Separador de Strings/Separador de string/Separador de string/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Separador_de_string
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btGerar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(37, 35);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(228, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // btGerar
+            // 
+            this.btGerar.Location = new System.Drawing.Point(111, 61);
+            this.btGerar.Name = "btGerar";
+            this.btGerar.Size = new System.Drawing.Size(75, 31);
+            this.btGerar.TabIndex = 1;
+            this.btGerar.Text = "Gerar Auto";
+            this.btGerar.UseVisualStyleBackColor = true;
+            this.btGerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(35, 14);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(129, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Digite seu nome completo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 104);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btGerar);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btGerar;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
+
---
+++ Projetos/Separador de Strings/Separador de string/Separador de string/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Separador_de_string
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNome != null)
+            {
+                var random = new Random(); // Define o random como novo Random... desnecessario
+                var nomeCompleto = txtNome.Text; // Define que o nome completo e o conteudo da caixa de texto
+                string[] partesNome = nomeCompleto.Split(' '); // Indica que as strings começam e acabam quando tem ' ' o espaço
+
+                // Usa o primeiro nome [0]
+                string nickName = partesNome[0] + '.';
+
+                //Se o numero de partes for maior que 1 ou seja 2
+                if (partesNome.Length > 1)
+                {
+                  //nickName + partes nome 
+                    nickName += partesNome[partesNome.Length -1];
+
+                  //Nome de usuario concantenado com numero automatico de 0 a 100 
+                    nickName += random.Next(1, 100);
+
+                    // apreseta o nome gerado em um pop-pup
+                    MessageBox.Show($"O nome gerado foi: {nickName}"); // $ indica um que pode passar uma intring
+                }
+
+                // senão, se ele for , menor ou igual a 1... (inserir um nome completo)
+                else if(partesNome.Length <= 1)
+                {
+                    MessageBox.Show("Insira seu nome completo");
+                }
+
+            }
+
+            // caso contrario (SE A CAIXA DE TEXTO ESTIVER VAZIA)...
+            else
+            {
+                MessageBox.Show("Por favor, insira seu nome completo.");
+            }
+        }
+    }
+}
